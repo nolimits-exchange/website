2366f31be70b5d0ff05717bcd86690fd7d7a809a
Mathew Davies <thepixeldeveloper@googlemail.com>
2017-01-09 22:54:41 +0000

    Add logging for coaster upload

--- src/AppBundle/Services/FileUploader.php
+++ src/AppBundle/Services/FileUploader.php
@@ -4,6 +4,7 @@ namespace Thepixeldeveloper\Nolimitsexchange\AppBundle\Services;
 
 use League\Flysystem\FileExistsException;
 use League\Flysystem\FilesystemInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 /**
@@ -17,17 +18,24 @@ class FileUploader implements FileUploaderInterface
      * @var FilesystemInterface
      */
     protected $ephemeral;
-
+    
+    /**
+     * @var LoggerInterface
+     */
+    protected $logger;
+    
     /**
      * FileUploader constructor.
      *
      * @param FilesystemInterface $ephemeral
+     * @param LoggerInterface     $logger
      */
-    public function __construct(FilesystemInterface $ephemeral)
+    public function __construct(FilesystemInterface $ephemeral, LoggerInterface $logger)
     {
         $this->ephemeral = $ephemeral;
+        $this->logger = $logger;
     }
-
+    
     /**
      * @inheritDoc
      *
@@ -38,9 +46,12 @@ class FileUploader implements FileUploaderInterface
         $filename = $id . '.' . $file->getClientOriginalExtension();
         $stream   = fopen($file->getRealPath(), 'rb+');
         
-        $this->ephemeral->writeStream($filename, $stream);
+        $written = $this->ephemeral->writeStream($filename, $stream);
         
         fclose($stream);
+        
+        $this->logger->info('Upload path', ['path' => $file->getRealPath()]);
+        $this->logger->critical('writeStream called', ['result' => $written]);
         
         return $filename;
     }
