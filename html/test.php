fcbebdc7b49b579664ea632a2635a33c0c3c09f8
Mathew Davies <thepixeldeveloper@googlemail.com>
2017-01-10 23:06:13 +0000

    Update test.php

--- html/test.php
+++ html/test.php
@@ -1,9 +1,8 @@
 <?php
 
-$dir = scandir('/tmp');
-
-print_r($dir);
-
-$dir = scandir('var/tmp/ephemeral');
-
-print_r($dir);
+if (file_put_contents("../var/tmp/ephemeral/foo", "bar") === false) {
+    echo "Cannot write";
+} else {
+    unlink("../ar/tmp/ephemeral/foo");
+    echo "Can write";
+}
