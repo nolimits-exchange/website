2cb33bffcb958583d7ca8742541a8e1da0e61840
Mathew Davies <thepixeldeveloper@googlemail.com>
2017-01-15 20:26:58 +0000

    Update the app_dev check

--- html/app_dev.php
+++ html/app_dev.php
@@ -4,6 +4,14 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
 $loader = require __DIR__.'/../app/autoload.php';
+
+$environment = getenv('SYMFONY_ENV');
+
+if ($environment === false || $environment !== 'dev') {
+    header('HTTP/1.0 403 Forbidden');
+    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+}
+
 Debug::enable();
 
 $kernel = new AppKernel('dev', true);
