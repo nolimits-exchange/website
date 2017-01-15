1dba15b8e4d6313de8f0f3230b1ff4f68536dc83
Mathew Davies <thepixeldeveloper@googlemail.com>
2017-01-15 12:59:54 +0000

    Update the test script

--- html/test.php
+++ html/test.php
@@ -1,5 +1,27 @@
 <?php
 
-$dir = scandir(__DIR__ . '/../var/tmp/ephemeral');
+echo "Current User";
 
-print_r($dir);
+var_dump(posix_getpwuid(posix_geteuid()));
+
+echo "Umask";
+
+var_dump(sprintf('%o', umask()));
+
+echo "var/tmp/ephemeral directory listing";
+
+$directory = __DIR__ . '/../var/tmp/ephemeral';
+
+$files = array_diff(
+    scandir($directory), ['.', '..']
+);
+
+foreach($files as $file) {
+    echo "Perms for " . $file;
+    
+    var_dump(fileperms($file));
+    
+    echo "User for " . $file;
+    
+    var_dump(fileowner($file));
+}
