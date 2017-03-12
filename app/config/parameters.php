a73ac2bcb441738eb406dd59b6f6826b1560a7c4
Mathew Davies <thepixeldeveloper@googlemail.com>
2017-03-12 17:52:53 +0000

    Move email environment variables from .secrets.json.

--- app/config/parameters.php
+++ app/config/parameters.php
@@ -7,12 +7,6 @@ $secrets = json_decode(file_get_contents($_SERVER['APP_SECRETS']), true);
 /**
  * @var $container Container
  */
-$container->setParameter('mailer_transport', 'smtp');
-$container->setParameter('mailer_host',     $secrets['CUSTOM']['EMAIL_HOST']);
-$container->setParameter('mailer_port',     $secrets['CUSTOM']['EMAIL_PORT']);
-$container->setParameter('mailer_user',     $secrets['CUSTOM']['EMAIL_USER']);
-$container->setParameter('mailer_password', $secrets['CUSTOM']['EMAIL_PASS']);
-
 $container->setParameter('secret', $secrets['CUSTOM']['COOKIE_SALT']);
 
 $container->setParameter('locale', 'en');
