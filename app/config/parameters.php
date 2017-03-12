cd1c954327a59ebe3220952f8de65910d9e73997
Mathew Davies <thepixeldeveloper@googlemail.com>
2017-03-12 17:38:03 +0000

    Remove MySQL configuration from the .secrets.json file.

--- app/config/parameters.php
+++ app/config/parameters.php
@@ -7,14 +7,6 @@ $secrets = json_decode(file_get_contents($_SERVER['APP_SECRETS']), true);
 /**
  * @var $container Container
  */
-
-$container->setParameter('database_driver', 'pdo_mysql'); 
-$container->setParameter('database_host',     $secrets['MYSQL']['HOST']);
-$container->setParameter('database_port',     $secrets['MYSQL']['PORT']);
-$container->setParameter('database_name',     $secrets['MYSQL']['DATABASE']);
-$container->setParameter('database_user',     $secrets['MYSQL']['USER']);
-$container->setParameter('database_password', $secrets['MYSQL']['PASSWORD']);
-
 $container->setParameter('mailer_transport', 'smtp');
 $container->setParameter('mailer_host',     $secrets['CUSTOM']['EMAIL_HOST']);
 $container->setParameter('mailer_port',     $secrets['CUSTOM']['EMAIL_PORT']);
